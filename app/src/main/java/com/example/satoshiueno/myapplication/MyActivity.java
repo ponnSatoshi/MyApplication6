930feb029ffd5faf1d55095090994a37762db6f0
satoshiueno <ueno.info@gmail.com>
2014-07-23 18:40:16 +0900

    hogehoge

--- app/src/main/java/com/example/satoshiueno/myapplication/MyActivity.java
+++ app/src/main/java/com/example/satoshiueno/myapplication/MyActivity.java
@@ -24,8 +24,6 @@ public class MyActivity extends ActionBarActivity {
                 //EditText editText = (EditText) findViewById(R.id.edittext);
                 //intent.putExtra("inputText", editText.getText().toString());
                 startActivity(intent);
-                startActivity(intent);
-
             }
         });
     }
